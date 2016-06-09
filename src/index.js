ca30843c83f519618de4984afde41f46497357f9
Thomas Heck <t@b128.net>
2016-06-09 13:45:11 +0200

    + some old code...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import CssModules from 'css-modules-loader-core';
 import path from 'path';
+import {readFile} from 'js-utils/fs';
 
 export default function rollupCssModules() {
 	const cssModules = new CssModules();
+
+	const tokensByFile = Object.create(null);
 
 	return {
 		async transform(code, id) {
@@ -11,8 +14,42 @@ export default function rollupCssModules() {
 			}
 
 			const relativePath = path.relative(process.cwd(), id);
+			const relativeDir = path.dirname(relativePath);
 
-			const {exportTokens} = await cssModules.load(code, relativePath, null);
+			async function fetchDep(_newPath) {
+				const newPath = _newPath.replace(/^["']|["']$/g, ''); // WTF?!
+				const filePath = path.resolve(relativeDir, newPath);
+
+				if (filePath in tokensByFile) {
+					return tokensByFile[filePath];
+				}
+
+				const newRelativePath = path.relative(process.cwd(), filePath);
+				const newCode = await readFile(filePath);
+
+				const {exportTokens} = await cssModules.load(
+					newCode,
+					newRelativePath,
+					undefined,
+					fetchDep
+				);
+
+				tokensByFile[filePath] = exportTokens;
+
+				return exportTokens;
+			}
+
+			let exportTokens = tokensByFile[relativePath];
+			if (!exportTokens) {
+				const res = await cssModules.load(
+					code,
+					relativePath,
+					undefined,
+					fetchDep
+				);
+				exportTokens = res.exportTokens;
+				tokensByFile[relativePath] = exportTokens;
+			}
 
 			return {
 				code: `export default ${JSON.stringify(exportTokens)};`,
